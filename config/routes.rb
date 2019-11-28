6fed37c13e69ef22a5b5558d2f115806007e79d8
Bernadette Davis <bsd231@nyu.edu>
2019-11-28 18:58:01 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update] #run "$rake routes" to see key/value (pathname/path) pairs and folder paths
 end
